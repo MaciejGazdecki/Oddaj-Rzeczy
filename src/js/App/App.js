661d95217c4666754473de21bf9e8052cbaa24ed
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-12-13 01:19:29 +0100

    eslint config index.js

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from "./app.modules.scss";
-import {BrowserRouter as Router, Switch, Link, Route} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "../Components/Home/home";
 
 function App () {
