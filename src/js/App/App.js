1e22ceea495897dd1bc623a1659fef5b81799236
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-12-13 01:01:41 +0100

    I started to create routing, and homepage component and subcomponents

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import style from "./app.modules.scss"
+import style from "./app.modules.scss";
+import {BrowserRouter as Router, Switch, Link, Route} from "react-router-dom";
+import Home from "../Components/Home/home";
 
 function App () {
     return (
-        <div className={style.red}>Development branch</div>
+        <Router>
+            <Switch>
+                <Route path="/" component={Home}/>
+            </Switch>
+        </Router>
     )
 }
 
---
+++ src/js/Components/Home/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './home.modules.scss';
+
+function Home() {
+    return (
+        <div>HomePage</div>
+    )
+}
+
+export default Home;
