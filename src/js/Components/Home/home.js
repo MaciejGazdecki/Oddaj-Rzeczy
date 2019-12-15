9a19bd10f6402599968a30fe0e369e57d1dc8a76
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-12-15 13:23:25 +0100

    I renamed componnent

--- src/js/Components/Home/home.js
+++ src/js/Components/Home/home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import style from './home.modules.scss';
 import Header from "./subcomponents/Header/header";
 import ThreeColumns from "./subcomponents/ThreeColumns/threeColumns";
-import About from "./subcomponents/About/about";
+import AboutIdea from "./subcomponents/About/aboutIdea";
 
 function Home() {
     return (
         <>
             <Header/>
             <ThreeColumns/>
-            <About/>
+            <AboutIdea/>
         </>
     )
 }
