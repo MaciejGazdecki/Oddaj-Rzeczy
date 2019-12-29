1382440e7749fabe71ebd0c3ffe6ed38ef42ed7e
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-12-29 16:28:15 +0100

    I created data part of component

---
+++ src/js/Components/Home/Foundation/Data/data.js
@@ -0,0 +1,108 @@
+export const data = [
+    [
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        },
+        {
+            name:'Fundacja Dbam o Zdrowie',
+            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
+            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+        }
+    ],
+    [
+        {
+            name: 'Organizacja: Lorem Ipsum',
+            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
+            items:'Ut, aliquam, purus, sit, amet'
+        },
+        {
+            name: 'Organizacja: Lorem Ipsum',
+            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
+            items:'Ut, aliquam, purus, sit, amet'
+        },
+        {
+            name: 'Organizacja: Lorem Ipsum',
+            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
+            items:'Ut, aliquam, purus, sit, amet'
+        },
+        {
+            name: 'Organizacja: Lorem Ipsum',
+            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
+            items:'Ut, aliquam, purus, sit, amet'
+        },
+        {
+            name: 'Organizacja: Lorem Ipsum',
+            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
+            items:'Ut, aliquam, purus, sit, amet'
+        },
+        {
+            name: 'Organizacja: Lorem Ipsum',
+            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
+            items:'Ut, aliquam, purus, sit, amet'
+        }
+    ],
+    [
+        {
+            name:'Zbiórka Lorem Ipsum',
+            mission:'Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.',
+            items:'Egestas, sed, tempus'
+        },
+        {
+            name:'Zbiórka Lorem Ipsum',
+            mission:'Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.',
+            items:'Egestas, sed, tempus'
+        },
+        {
+            name:'Zbiórka Lorem Ipsum',
+            mission:'Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.',
+            items:'Egestas, sed, tempus'
+        }
+    ]
+];
--- src/js/Components/Home/Foundation/foundation.js
+++ src/js/Components/Home/Foundation/foundation.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./foundation.modules.scss";
 import Decoration from "../../../../images/Decoration.png";
-import {Link} from "react-router-dom";
+import {data} from "./Data/data";
 
 function Foundation() {
+    const [index, setIndex] = useState(0);
+
     return (
         <section className={style.foundation}>
             <div className={style.container}>
@@ -14,23 +16,38 @@ function Foundation() {
                     <div>
                         <img src={Decoration} alt="dekoracja"/>
                     </div>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+                    <div>
+                        <div onClick={() => setIndex(0)}>
+                            <p>
+                                Fundacjom
+                            </p>
+                        </div>
+                        <div onClick={()=> setIndex(1)}>
+                            <p>
+                                Organizacjom pozarządowym
+                            </p>
+                        </div>
+                        <div onClick={() => setIndex(2)}>
+                            <p>
+                                Lokalnym zbiórkom
+                            </p>
+                        </div>
+                    </div>
                     <div>
                         <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy.
                             Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
                         </p>
                     </div>
                 </div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
+                <ul>
+                    {data[index].map((el,index) =>
+                        <li key={index}>
+                            <p>{el.name}</p>
+                            <p>{el.mission}</p>
+                            <p>{el.items}</p>
+                        </li>
+                    )}
+                </ul>
             </div>
         </section>
     )
