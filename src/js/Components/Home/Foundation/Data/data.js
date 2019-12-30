d5636b537fcd35f8694ff264f1418c57c1f02600
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2019-12-30 03:22:07 +0100

    pagination done without styles

--- src/js/Components/Home/Foundation/Data/data.js
+++ src/js/Components/Home/Foundation/Data/data.js
@@ -6,52 +6,52 @@ export const data = [
             items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
         },
         {
-            name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+            name:'Fundacja “Dla dzieci”',
+            mission:'Cel i misja: Pomoc dzieciom z ubogich rodzin.',
+            items:'ubrania, meble, zabawki'
+        },
+        {
+            name:'Fundacja “Bez domu”',
+            mission:'Cel i misja: Pomoc dla osób nie posiadających miejsca zamieszkania.',
+            items:'ubrania, jedzenie, ciepłe koce'
+        },
+        {
+            name:'Fundacja Dbam o Zwierzęta',
+            mission:'Cel i misja: Pomoc schroniskom dla zwierząt',
+            items:'ubrania, jedzenie, koce'
+        },
+        {
+            name:'Fundacja Dbam o Zycie',
+            mission:'Cel i misja: bnhsdfd sfdsdfs zzczxc asfsdfsd',
+            items:'asda, asdad, asdas AGD, asdasda, asddasdas'
+        },
+        {
+            name:'Fundacja Dbam o sadadad',
+            mission:'Cel i misja: asdad sadasd asdasd asdad w asdd sda daasda',
+            items:'adadsd, sadad, asda AGD, asdaasd, asdad'
+        },
+        {
+            name:'Fundacja Dbam o sadasd',
+            mission:'Cel i misja: asdad asdasd asdad asd w asd sada sadadad',
+            items:'sadd, asdad, asda AGD, sadasd, asdads'
+        },
+        {
+            name:'Fundacja Dbam o gerssddf',
+            mission:'Cel i misja: Podsfsd dsfsd znajduyhtdr dsfdsfsd htertweafsd',
+            items:'gdfgdfs, jedzwfgreenie, sprerdgs, meeqfsdable, zabytrgaw'
         },
         {
             name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+            mission:'Cel i misja: Tristique senectus et netus et malesuada fames ac turpis ',
+            items:'Metus, dictum, at, tempor, commodo, ullamcorper'
         },
         {
-            name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
+            name:'Fundacja Dbam o bleble',
+            mission:'Cel i misja: Dictumst quisque sagittis purus sit',
+            items:'Lectus, quam, id,leo, in, vitae'
         },
         {
-            name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-        },
-        {
-            name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-        },
-        {
-            name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-        },
-        {
-            name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-        },
-        {
-            name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-        },
-        {
-            name:'Fundacja Dbam o Zdrowie',
-            mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
-            items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
-        },
-        {
-            name:'Fundacja Dbam o Zdrowie',
+            name:'Fundacja Dbam o misie',
             mission:'Cel i misja: Pomoc osobom znajdującym się w trudnej sytuacji życiowej',
             items:'ubrania, jedzenie, sprzęt AGD, meble, zabawki'
         }
@@ -63,46 +63,46 @@ export const data = [
             items:'Ut, aliquam, purus, sit, amet'
         },
         {
-            name: 'Organizacja: Lorem Ipsum',
-            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
-            items:'Ut, aliquam, purus, sit, amet'
+            name: 'Organizacja: Umcia Umcia',
+            mission: 'Nisl rhoncus mattis rhoncus urna neque.',
+            items:'Elementum, pulvinar, etiam, non, quam'
         },
         {
-            name: 'Organizacja: Lorem Ipsum',
+            name: 'Organizacja: Ampa ampa',
             mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
-            items:'Ut, aliquam, purus, sit, amet'
+            items:'Ullamcorper, eget, nulla, facilisi, etiam'
         },
         {
-            name: 'Organizacja: Lorem Ipsum',
-            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
-            items:'Ut, aliquam, purus, sit, amet'
+            name: 'Organizacja: Baila baila',
+            mission: 'Vel fringilla est ullamcorper eget nulla facilisi etiam',
+            items:'Natoque, penatibus, et, magnis'
         },
         {
-            name: 'Organizacja: Lorem Ipsum',
+            name: 'Organizacja: Trele morele',
             mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
-            items:'Ut, aliquam, purus, sit, amet'
+            items:'Amet, nisl, suscipit, adipiscing'
         },
         {
-            name: 'Organizacja: Lorem Ipsum',
-            mission: 'Quis varius quam quisque id diam vel quam elementum pulvinar.',
-            items:'Ut, aliquam, purus, sit, amet'
+            name: 'Organizacja: Miau miau',
+            mission: 'Phasellus faucibus scelerisque eleifend donec pretium',
+            items:'Proin, nibh, nisl, condimentum, id'
         }
     ],
     [
         {
-            name:'Zbiórka Lorem Ipsum',
+            name:'Zbiórka na piwo',
+            mission:'Cum sociis natoque penatibus et magnis',
+            items:'met, nisl, suscipit'
+        },
+        {
+            name:'Zbiórka Lupa lupa',
             mission:'Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.',
             items:'Egestas, sed, tempus'
         },
         {
-            name:'Zbiórka Lorem Ipsum',
-            mission:'Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.',
-            items:'Egestas, sed, tempus'
-        },
-        {
-            name:'Zbiórka Lorem Ipsum',
-            mission:'Hendrerit gravida rutrum quisque non tellus orci ac auctor augue.',
-            items:'Egestas, sed, tempus'
+            name:'Zbiórka fafu fafu',
+            mission:'Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus.',
+            items:'Vitae, purus, faucibus'
         }
     ]
 ];
--- src/js/Components/Home/Foundation/foundation.js
+++ src/js/Components/Home/Foundation/foundation.js
@@ -5,6 +5,13 @@ import {data} from "./Data/data";
 
 function Foundation() {
     const [index, setIndex] = useState(0);
+    const [page, setPage] = useState(1);
+    const perPage = 3;
+
+    const pages = [];
+    for (let i = 1; i <= data[index].length/perPage; i++) {
+        pages.push(<li key={i} onClick={() => setPage(i)}>{i}</li>)
+    }
 
     return (
         <section className={style.foundation}>
@@ -16,18 +23,18 @@ function Foundation() {
                     <div>
                         <img src={Decoration} alt="dekoracja"/>
                     </div>
-                    <div>
-                        <div onClick={() => setIndex(0)}>
+                    <div className={style.buttons}>
+                        <div className={style.button} onClick={() => {setIndex(0);setPage(1)}}>
                             <p>
                                 Fundacjom
                             </p>
                         </div>
-                        <div onClick={()=> setIndex(1)}>
+                        <div className={style.button} onClick={()=> {setIndex(1);setPage(1)}}>
                             <p>
                                 Organizacjom pozarządowym
                             </p>
                         </div>
-                        <div onClick={() => setIndex(2)}>
+                        <div className={style.button} onClick={() => {setIndex(2);setPage(1)}}>
                             <p>
                                 Lokalnym zbiórkom
                             </p>
@@ -39,14 +46,17 @@ function Foundation() {
                         </p>
                     </div>
                 </div>
-                <ul>
-                    {data[index].map((el,index) =>
+                <ul className={style.items}>
+                    {data[index].slice(page*perPage-perPage,page*perPage).map((el,index) =>
                         <li key={index}>
                             <p>{el.name}</p>
                             <p>{el.mission}</p>
                             <p>{el.items}</p>
                         </li>
                     )}
+                </ul>
+                <ul className={style.pages} style={data[index].length === 3 ? {display:"none"} : {display:"flex"}}>
+                    {pages}
                 </ul>
             </div>
         </section>
