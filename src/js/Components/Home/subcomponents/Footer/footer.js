8640984c9234204e321962bcfb5a544a5e924371
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2020-01-03 20:25:38 +0100

    I made form validation

--- src/js/Components/Home/subcomponents/Footer/footer.js
+++ src/js/Components/Home/subcomponents/Footer/footer.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import style from "./footer.modules.scss";
 import {Element} from "react-scroll";
 import Decoration from "../../../../../images/Decoration.png";
 import Background from "../../../../../images/Background-Contact-Form.jpg";
 import Facebook from "../../../../../images/Facebook.png";
-import Instagram from "../../../../../images/Instagram.png"
+import Instagram from "../../../../../images/Instagram.png";
+import {useForm} from 'react-hook-form';
+import axios from "axios";
 
 function Footer() {
+    const {register, handleSubmit, errors} = useForm();
+
+    const onSubmit = async (data) => {
+        event.preventDefault();
+        await axios.post("https://fer-api.coderslab.pl/v1/portfolio/contact", {...data})
+            .then(res => {console.log(res); alert("wysłano wiadomość")})
+            .catch(err => console.log(err));
+    };
+
+
     return (
         <Element>
             <section className={style.footer} style={{backgroundImage:`url(${Background})`}}>
@@ -19,32 +31,56 @@ function Footer() {
                                 </p>
                                 <img src={Decoration} alt=""/>
                             </div>
-                            <form className={style.form}>
+                            <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
                                 <div className={style.inputs}>
                                     <label>
                                         <p>Wpisz swoje imię</p>
                                         <input
                                             type="text"
                                             placeholder="Podaj imię"
+                                            name="name"
+                                            ref={register({required:true, pattern: /^\S+$/ })}
                                         />
+                                        {errors.name && errors.name.type === 'required' && (
+                                            <p className={style.error}>To pole jest wymagane</p>
+                                        )}
+                                        {errors.name && errors.name.type === 'pattern' && (
+                                            <p className={style.error}>To pole nie może zawierać spacji</p>
+                                        )}
                                     </label>
                                     <label>
                                         <p>Wpisz swój email</p>
                                         <input
                                             type="text"
                                             placeholder="podaj email"
+                                            name="email"
+                                            ref={register({required:true, pattern: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/})}
                                         />
+                                        {errors.email && errors.email.type === 'required' && (
+                                            <p className={style.error}>To pole jest wymagane</p>
+                                        )}
+                                        {errors.email && errors.email.type === 'pattern' && (
+                                            <p className={style.error}>NIepoprawny format email</p>
+                                        )}
                                     </label>
                                 </div>
                                 <div className={style.textarea}>
                                     <p>Wpisz swoją wiadomość</p>
                                     <textarea
-                                        name=""
                                         id=""
                                         cols="30"
                                         rows="10"
-                                        placeholder="wpisz swoją wiadomość">
+                                        placeholder="Tutaj wpisz swoją wiadomość"
+                                        name="message"
+                                        ref={register({required:true, minLength: 120})}
+                                    >
                                     </textarea>
+                                    {errors.message && errors.message.type === 'required' && (
+                                        <p className={style.error}>To pole jest wymagane</p>
+                                    )}
+                                    {errors.message && errors.message.type === 'minLength' && (
+                                        <p className={style.error}>Minimalna długość wiadomości to 120 znaków</p>
+                                    )}
                                 </div>
                                 <div className={style.submit}>
                                     <input
