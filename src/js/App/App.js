8426cdb3ddc201be893c742e5edb97cce178dfb8
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2020-01-10 01:38:33 +0100

    LogOut section created

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -5,6 +5,7 @@ import Home from "../Components/Home/home";
 import Login from "../Components/Login/login";
 import Register from "../Components/Register/register";
 import Navigation from "../Components/Navigation/navigation";
+import Logout from "../Components/Logout/logout";
 
 function App () {
     return (
@@ -14,6 +15,7 @@ function App () {
                 <Route path="/" exact component={Home}/>
                 <Route path="/logowanie" component={Login}/>
                 <Route path="/rejestracja" component={Register}/>
+                <Route path="/wylogowano" component={Logout}/>
             </div>
         </Router>
     )
