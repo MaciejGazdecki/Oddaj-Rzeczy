2542a8fa3181e8991641f71f1f16e65699a7d748
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2020-01-19 02:36:54 +0100

    I modified UI depending on it if user is loggedIn or not

--- src/js/Components/Navigation/SmallNavigation/smallNavigation.js
+++ src/js/Components/Navigation/SmallNavigation/smallNavigation.js
@@ -2,9 +2,20 @@ import React, {useContext} from "react";
 import {NavLink} from "react-router-dom";
 import style from "./smallNavigation.modules.scss"
 import {UserContext} from "../../../App/userContext";
+import firebase from "firebase";
 
 function SmallNavigation() {
     const user = useContext(UserContext);
+
+    const logOut = () => {
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+            alert('User Logged Out!');
+        }).catch(function(error) {
+            // An error happened.
+            alert(error);
+        });
+    };
     return (
         <ul className={style.loginNav}>
             <li>
@@ -13,9 +24,8 @@ function SmallNavigation() {
                 </NavLink>
             </li>
             <li>
-                <NavLink activeClassName={style.active} to="/rejestracja">
-                    Załóż konto
-                </NavLink>
+                {!user ? <NavLink activeClassName={style.active} to="/rejestracja">Załóż konto</NavLink> :
+                    <button className={style.logout} onClick={logOut}>Wyloguj się</button>}
             </li>
         </ul>
     )
